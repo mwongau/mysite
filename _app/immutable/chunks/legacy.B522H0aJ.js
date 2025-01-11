8eb727b4525f1f46d6f54a242dbd2991736cd1a4
M Wong <wongmt15@gmail.com>
2025-01-12 05:53:46 +1100

    Updates

---
+++ _app/immutable/chunks/legacy.B522H0aJ.js
@@ -0,0 +1,1 @@
+import{e}from"./runtime.wV5AR2Er.js";e();
---
+++ _app/immutable/nodes/6.x78npJdo.js
@@ -0,0 +1,7 @@
+import{a as o,t as i}from"../chunks/disclose-version.DBXtjPK8.js";import"../chunks/legacy.B522H0aJ.js";import{n as s}from"../chunks/runtime.wV5AR2Er.js";var a=i(`<h1 class="svelte-3i4bev">Privacy policy</h1> <h2 class="svelte-3i4bev">My contact</h2> <p class="svelte-3i4bev">Email: mwong[at]mwongsite.com, replace [at] by @</p> <p class="svelte-3i4bev">If you contact the site author by email, your name, 
+		email address and other information you provide to the 
+		site author will be stored in the email account of the site 
+		author.</p> <h2 class="svelte-3i4bev">Cookies</h2> <p class="svelte-3i4bev">This website might use cookies to improve your experience of 
+	using the site. If you continue to use this site, it will be 
+	assumed that you agree to the use of cookies.</p> <p class="svelte-3i4bev">The above privacy policy can be changed at any time by 
+	the site author without giving notice.</p>`,1);function l(e){var t=a();s(12),o(e,t)}export{l as component};
