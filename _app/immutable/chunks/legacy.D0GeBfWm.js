90b6771ce3477f2452fb1524debf69617cf900d7
M Wong <wongmt15@gmail.com>
2025-01-13 00:32:19 +1100

    Updates

---
+++ _app/immutable/chunks/legacy.D0GeBfWm.js
@@ -0,0 +1,1 @@
+import{i as a}from"./runtime.D_dS6ku-.js";a();
---
+++ _app/immutable/nodes/3.BS4pK7MX.js
@@ -0,0 +1,1 @@
+import{a as p,t as e}from"../chunks/disclose-version.hn53xuVD.js";import"../chunks/legacy.D0GeBfWm.js";import{n as a}from"../chunks/runtime.D_dS6ku-.js";var r=e('<h1>Blog</h1> <p>Select post</p> <p><a href="/mysite/blog/1">About this blog</a></p> <p><a href="/mysite/blog/2">Why did I develop this blog</a></p>',1);function l(o){var t=r();a(6),p(o,t)}export{l as component};
